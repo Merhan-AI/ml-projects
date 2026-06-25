819d2369cf5e252ab3243c755e4e1131d5f00b0c
Mohammed Mehran <shaikhmohammedmehran@gmail.com>
2026-06-25 16:00:04 +0530

    Add files via upload

---
+++ salary_prediction.py
@@ -0,0 +1,10 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+Experience =np.array([1, 2, 3, 4, 5, 6, 7, 8]).reshape(-1,1)
+Salary = np.array([30000, 35000, 40000, 50000, 60000, 70000, 80000, 90000])
+model = LinearRegression()
+model.fit(Experience,Salary)
+prediction = model.predict([[9]])
+print(prediction)
+print(model.coef_)
+print(model.intercept_)
