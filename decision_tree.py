69da5b131aac89d3687d6b1e3da0fded7d357a60
Mohammed Mehran <shaikhmohammedmehran@gmail.com>
2026-06-25 17:55:31 +0530

    Add files via upload

---
+++ decision_tree.py
@@ -0,0 +1,19 @@
+import numpy as np 
+from sklearn.tree import DecisionTreeClassifier 
+x = np.array([[2, 300],
+              [4, 500],
+              [6, 600],
+              [8, 700],
+              [10, 800],
+              [3, 400],
+              [7, 750],
+              [5, 550]])
+Loan_approved = np.array([0, 0, 1, 1, 1, 0, 1, 0])
+model = DecisionTreeClassifier()
+model.fit(x,Loan_approved)
+print(model.predict([[25, 800]]))
+from sklearn.metrics import accuracy_score
+print(accuracy_score(Loan_approved,model.predict(x)))
+from sklearn.tree import export_text
+tree_rules = export_text(model, feature_names=["Income", "Credit Score"])
+print(tree_rules)
